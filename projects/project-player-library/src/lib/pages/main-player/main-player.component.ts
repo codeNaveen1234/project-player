d1abdd2c055fecb9a9783f739ea267eee2050f61
Vinod-V3 <vinod@tunerlabs.com>
2024-04-02 10:55:59 +0530

    Routing implementation for details-page

--- projects/project-player-library/src/lib/pages/main-player/main-player.component.ts
+++ projects/project-player-library/src/lib/pages/main-player/main-player.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'lib-main-player',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPlayerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.navigate()
+  }
+
+  navigate(){
+    this.router.navigate(['/details'],{skipLocationChange:true})
   }
 
 }
--- projects/project-player-library/src/lib/project-player-library.module.ts
+++ projects/project-player-library/src/lib/project-player-library.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { ProjectPlayerLibraryComponent } from './project-player-library.component';
 import { MainPlayerComponent } from './pages/main-player/main-player.component';
+import { DetailsPageComponent } from './pages/details-page/details-page.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  { path:'details', component:DetailsPageComponent }
+]
 
 @NgModule({
   declarations: [
     ProjectPlayerLibraryComponent,
-    MainPlayerComponent
+    MainPlayerComponent,
+    DetailsPageComponent
   ],
   imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
   exports: [
-    MainPlayerComponent
+    RouterModule
   ]
 })
 export class ProjectPlayerLibraryModule { }
