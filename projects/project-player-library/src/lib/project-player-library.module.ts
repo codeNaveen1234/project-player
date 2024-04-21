fd0652a690f84fdfff077d37c1c753de52e308fe
Vinod-V3 <vinod@tunerlabs.com>
2024-04-21 23:01:22 +0530

    Implemented translation module

--- projects/project-player-library/src/lib/project-player-library.module.ts
+++ projects/project-player-library/src/lib/project-player-library.module.ts
@@ -4,10 +4,17 @@ import { MainPlayerComponent } from './pages/main-player/main-player.component';
 import { DetailsPageComponent } from './pages/details-page/details-page.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
   { path:'details', component:DetailsPageComponent }
 ]
+
+export function translateHttpLoaderFactory (httpClient: HttpClient){
+  return new TranslateHttpLoader(httpClient);
+}
 
 @NgModule({
   declarations: [
@@ -17,10 +24,27 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader:{
+        provide : TranslateLoader,
+        useFactory: translateHttpLoaderFactory,
+        deps : [HttpClient],
+      },
+    })
   ],
   exports: [
     RouterModule
   ]
 })
-export class ProjectPlayerLibraryModule { }
+export class ProjectPlayerLibraryModule {
+  constructor(private translate: TranslateService){
+    this.setLanguage()
+  }
+
+  setLanguage(){
+    this.translate.setTranslation('en',require('./assets/i18n/en.json'))
+    this.translate.setDefaultLang('en')
+  }
+}
