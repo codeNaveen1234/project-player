026643d0d439a782f5c6ae277dad36def79f63db
Vinod-V3 <vinod@tunerlabs.com>
2024-04-30 12:19:55 +0530

    Tailwind css integration done

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ['./projects/demo-app/src/**/*.{html,ts}','./projects/project-player-library/src/**/*.{html,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
