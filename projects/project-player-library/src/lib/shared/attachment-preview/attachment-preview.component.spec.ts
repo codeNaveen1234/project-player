e518a4ec1fa6e89b96c5b572ab5fa5dd001248d5
Vinod V <vinod@tunerlabs.com>
2024-06-26 19:48:17 +0530

    Changes done to preview the attachments

---
+++ projects/project-player-library/src/lib/shared/attachment-preview/attachment-preview.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AttachmentPreviewComponent } from './attachment-preview.component';
+
+describe('AttachmentPreviewComponent', () => {
+  let component: AttachmentPreviewComponent;
+  let fixture: ComponentFixture<AttachmentPreviewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AttachmentPreviewComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(AttachmentPreviewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
